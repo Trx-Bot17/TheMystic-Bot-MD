1b422d5a0eab539767c332cb1be7fa1e4aa462b6
Trx-Bot17 <164639837+Trx-Bot17@users.noreply.github.com>
2024-04-05 22:56:43 -0600

    config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import fetch from 'node-fetch';
 import axios from 'axios';
 import moment from 'moment-timezone';
 
-global.botnumber = "+527822194233"
+global.botnumber = ""
 global.confirmCode = ""
 
 global.owner = [
